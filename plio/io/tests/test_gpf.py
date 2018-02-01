7aac3c31ed30f1c639e644b4fa6db0d173de2b0b
thareUSGS <thare@usgs.gov>
2018-01-31 22:18:08 -0700

    save_gpf seems to be working well. Note sure the test is ready though. It is there but uncertain if filecmp.cmp with shallow=True will allow it to pass since they are not exactly the same (e.g. number of decimal places). Also cat file be written too?

--- plio/io/tests/test_gpf.py
+++ plio/io/tests/test_gpf.py
@@ -3,13 +3,19 @@ import pandas as pd
 from pandas.util.testing import assert_frame_equal
 
 from plio.io.io_gpf import read_gpf
+from plio.io.io_gpf import save_gpf
 from plio.examples import get_path
 
+import filecmp
 import pytest
 
 @pytest.fixture
+
 def insight_gpf():
     return get_path('InSightE08_XW.gpf')
+
+def out_insight_gpf():
+    return get_path('out_InSightE08_XW.gpf')
 
 @pytest.fixture()
 def insight_expected():
@@ -19,3 +25,10 @@ def insight_expected():
 def test_read_gfp(gpf, expected):
     df = read_gpf(gpf)
     assert_frame_equal(df, expected)
+
+@pytest.mark.parametrize('gpf, out_gpf', [(insight_gpf(),out_insight_gpf())])
+def test_write_gfp(gpf, out_gpf):
+    df = read_gpf(gpf)
+    val = save_gpf(df, out_gpf)
+    assertTrue(filecmp.cmp(gpf, out_gpf, shallow=True), "save_gpf does not create an equal file.")
+
