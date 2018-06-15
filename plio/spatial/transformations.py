36c3c5ff61f509f05f91d21bf7a62104b431e46b
Adam Paquette <acpaquette@usgs.gov>
2018-06-15 09:59:36 -0700

    Updated reproj doc string.

--- plio/spatial/transformations.py
+++ plio/spatial/transformations.py
@@ -173,8 +173,15 @@ def get_axis(file):
 
 def reproject(record, semi_major, semi_minor, source_proj, dest_proj, **kwargs):
     """
-    Transforms a set of coordinates (y, x, z) from one coordinate system to a different
-    coordinate system. Also handles ocentric to ographic transformations, and vise versa.
+    Thin wrapper around PyProj's Transform() function to transform 1 or more three-dimensional
+    point from one coordinate system to another. If converting between Cartesian
+    body-centered body-fixed (BCBF) coordinates and Longitude/Latitude/Altitude coordinates,
+    the values input for semi-major and semi-minor axes determine whether latitudes are
+    planetographic or planetocentric and determine the shape of the datum for altitudes.
+    If semi_major == semi_minor, then latitudes are interpreted/created as planetocentric
+    and altitudes are interpreted/created as referenced to a spherical datum.
+    If semi_major != semi_minor, then latitudes are interpreted/created as planetographic
+    and altitudes are interpreted/created as referenced to an ellipsoidal datum.
 
     Parameters
     ----------
