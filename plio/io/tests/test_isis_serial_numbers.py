14a56dc7bdb969b104e9c0319d0197dd569daf20
Jay <jlaura@asu.edu>
2020-03-13 20:19:50 -0700

    Testing #142

--- plio/io/tests/test_isis_serial_numbers.py
+++ plio/io/tests/test_isis_serial_numbers.py
@@ -11,7 +11,8 @@ def label(request):
     return get_path(request.param)
 
 @pytest.mark.parametrize("label, expected", [('Test_PVL.lbl', 'APOLLO15/METRIC/1971-07-31T14:02:27.179'),
-                                             ('ctx.pvl','MRO/CTX/0906095311:038')
+                                             ('ctx.pvl','MRO/CTX/0906095311:038'),
+                                             ('testing_142.pvl', 'MRO/CTX/1153242990:243')
                                              ], indirect=['label'])
 def test_generate_serial_number(label, expected):
     serial = isis_serial_number.generate_serial_number(label)
